d904a4d1411e8d4c0580335099a9c19f1398f034
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-15 13:17:38 +0200

    feat: lower navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { LowerNavbarComponent } from './lower-navbar/lower-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroSectionComponent
+    HeroSectionComponent,
+    LowerNavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/lower-navbar/lower-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lower-navbar',
+  templateUrl: './lower-navbar.component.html',
+  styleUrls: ['./lower-navbar.component.scss']
+})
+export class LowerNavbarComponent {
+
+  constructor() { }
+}
