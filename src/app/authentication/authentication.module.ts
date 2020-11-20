0603e58f4258a37f6a40908c9fe4f7491f0a770b
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-20 22:50:24 +0200

    feat: auth service

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { AuthenticationService } from './authentication.service';
-import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,7 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthenticationService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data: {username: string, password: string}): Observable<any> {
+    return this.http.post(`${environment.apiURL}/login`, data, this.httpOptions);
+  }
+
+  register(data: {username: string, password: string, repeatPassword: string}): Observable<any> {
+    return this.http.post(`${environment.apiURL}/register`, data, this.httpOptions);
+  }
+
+  logout(): Observable<any> {
+    return this.http.get(`${environment.apiURL}\logout`);
+  }
 }
