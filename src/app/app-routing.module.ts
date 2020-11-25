b91c83856f6a61521e4b81a8ceb57d0a0d07a5d8
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-25 15:14:11 +0200

    feat: product details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductComponent } from './products/product/product.component';
 import { ProductsComponent } from './products/products/products.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'cigars', component: ProductsComponent},
+  { path: 'cigars', component: ProductsComponent },
+  { path: 'product/:id', component: ProductComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
