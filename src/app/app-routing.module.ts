eab2f044454f7319bfaf4f3749f9c3551d1a3da9
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-11-29 00:20:42 +0200

    feat: user pages routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './authentication/login/login.component';
 import { RegisterComponent } from './authentication/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductComponent } from './products/product/product.component';
 import { ProductsComponent } from './products/products/products.component';
+import { CartComponent } from './user/cart/cart.component';
+import { ProfileComponent } from './user/profile/profile.component';
+import { SettingsComponent } from './user/settings/settings.component';
+import { WishlistComponent } from './user/wishlist/wishlist.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
@@ -13,6 +18,10 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'cigars', component: ProductsComponent },
   { path: 'product/:id', component: ProductComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'wishlist', component: WishlistComponent },
+  { path: 'cart', component: CartComponent },
   { path: '**', component: NotFoundComponent }
 ];
 
