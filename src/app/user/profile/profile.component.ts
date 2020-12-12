67d8188a55f0fac11dec13576ec693bf00d50eba
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2020-12-12 16:14:06 +0200

    feat: change password validation extended

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { AuthenticationService } from 'src/app/authentication/authentication.service';
 import { IUser } from 'src/app/shared/interfaces/user';
 import { UserService } from '../user.service';
@@ -10,6 +12,7 @@ import { UserService } from '../user.service';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  error: string;
   user: IUser;
   constructor(
     private userService: UserService,
@@ -23,7 +26,14 @@ export class ProfileComponent implements OnInit {
   }
 
   changePasswordHandler(value: object): void {
-    this.userService.changePassword(value).subscribe(() => {
+    this.userService.changePassword(value).pipe(
+      catchError(err => {
+        if (!err.ok) {
+          this.error = err.error;
+          return throwError(err);
+        }
+      })
+    ).subscribe(() => {
       this.authService.logout().subscribe();
       this.router.navigate(['/']);
     });
