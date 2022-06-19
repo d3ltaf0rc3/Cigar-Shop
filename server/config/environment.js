550182167cf511c00e222fc1323db5379f86e9d1
Martin Marinov <59050469+d3ltaf0rc3@users.noreply.github.com>
2022-06-19 16:12:46 +0300

    update packages anc change cookie config

--- server/config/environment.js
+++ server/config/environment.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV === "production") {
     module.exports = {
         cookieOptions: {
             expires: new Date(Date.now() + 604800000),
+            httpOnly: true,
             sameSite: "None",
             secure: true
         }
