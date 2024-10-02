ead4ac804c9dee9fd4b74643a54778f481901185
Martin Marinov <sunrise_martin@hotmail.com>
2024-10-02 12:30:12 +0300

    upgrade dependencies and angular to v15

--- src/app/authentication/auth.guard.ts
+++ src/app/authentication/auth.guard.ts
@@ -6,7 +6,7 @@ import { AuthenticationService } from './authentication.service';
 export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthenticationService) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(_: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     if (!!this.authService.user) {
       if (state.url === '/login' || state.url === '/register') {
         this.router.navigate(['/']);
